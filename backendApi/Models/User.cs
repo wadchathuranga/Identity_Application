0668275b631336039f734dab0667b9faa48109e4
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-09 10:11:22 +0530

    user model created

---
+++ backendApi/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace backendApi.Models
+{
+    public class User : IdentityUser
+    {
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
