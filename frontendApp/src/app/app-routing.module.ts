1784d167c51215db419c9189063b14e871f008eb
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-11 17:21:15 +0530

    new route added

--- frontendApp/src/app/app-routing.module.ts
+++ frontendApp/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './shared/components/errors/not-found/not-found.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'products', component: ProductComponent },
   // Implenting lazy loading by the following format
   {
     path: 'account',
