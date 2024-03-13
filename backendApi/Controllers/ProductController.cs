168023b8611a4c08978b45295f7f46ff6ab36067
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-13 22:58:27 +0530

    product controller added

---
+++ backendApi/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backendApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("get-products")]
+        public IActionResult Products() 
+        {
+            return Ok(new JsonResult(new { message = "Only authorize users can view products" }));
+        }
+    }
+}
