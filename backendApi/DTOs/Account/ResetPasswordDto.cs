f28bc073a42afbd0afedb2b1cf59d2850a1a3f20
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-14 20:40:47 +0530

    reset pswd dto added

---
+++ backendApi/DTOs/Account/ResetPasswordDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backendApi.DTOs.Account
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "New Password must be at least {2}, and maximum {1} characters")]
+        public string NewPassword { get; set; }
+    }
+}
