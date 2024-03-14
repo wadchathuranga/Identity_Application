e3fad6cd415d5834c820cc0c1c09c890def2a30a
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-14 19:41:30 +0530

    reg dto updated

--- backendApi/DTOs/Account/RegisterDto.cs
+++ backendApi/DTOs/Account/RegisterDto.cs
@@ -13,7 +13,7 @@ namespace backendApi.DTOs.Account
         public string LastName { get; set; }
 
         [Required]
-        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage = "Invalid email address")]
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
         [Required]
